737efc284abfba8506ad1edb5b808b05a868eac4
Diviyavs08 <36121442+Diviyavs08@users.noreply.github.com>
2018-02-04 13:04:53 +0530

    Add files via upload

---
+++ beginner/declare 10 variables in arithmetic operation.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     04/02/2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=10
+b=2
+c=3
+d=4
+e=6
+f=5
+g=10
+h=5
+i=4
+j=5
+print(a+b+c+d+e+f+g+h+i+j)
+print(a*b*c*d*e*f*g*h*i*j)
+print(a/b/c/d/e/f/g/h/i/j)
